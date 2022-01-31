4eced3b64f43f414a9e712343198d1e6c2a6106d
daniap14 <xboxx105@gmail.com>
2022-01-31 13:41:43 +0100

    1

---
+++ Assets/Scripts/playbutton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class playbutton : MonoBehaviour
+{
+   public void PlayButton()
+    {
+        SceneManager.LoadScene("Ville");
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+}
